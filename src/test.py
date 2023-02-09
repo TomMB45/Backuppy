ed14fda5b76467ec233f37eb2f0c35a368a56b5c
MAUGER-BIROCHEAU Tom <tom.mauger-birocheau@etu.univ-cotedazur.fr>
2023-02-09 21:14:55 +0100

    update

--- src/test.py
+++ src/test.py
@@ -2,8 +2,9 @@ import tkinter
 from tkinter import filedialog
 import os 
 import shutil
+import tqdm
 
-def parcourir(title="Choisir un dossier") : 
+def parcourir(src = True) : 
     """
     Function allowing to browse folders and choose a folder
     
@@ -18,13 +19,13 @@ def parcourir(title="Choisir un dossier") :
     """
 
     tkinter.Tk().withdraw() # prevents an empty tkinter window from appearing
-    folder_path = filedialog.askdirectory(initialdir="D:", title=title)
+    if src : 
+        folder_path = filedialog.askdirectory(initialdir="C:\\Users\\Tom\\Documents\\", title="Choose a folder to copy")
+    else : 
+        folder_path = filedialog.askdirectory(initialdir="D:", title="Choose a folder to paste")
     return folder_path
 
-
-import glob
-
-def check_folder(folder_path, dest_folder) : 
+def check_folder(src_folder_path, dest_folder_path) : 
     """
     Recursive function to copy a folder and its content
 
@@ -37,26 +38,18 @@ def check_folder(folder_path, dest_folder) :
     -------
     None
     """
-    files = os.listdir(folder_path)
+    files = os.listdir(src_folder_path)
     for file in files :
-        if os.path.isfile(os.path.join(folder_path, file)) :
-            print(file, "test") 
-        else : 
-            os.chdir(os.path.join(folder_path, file))
-            os.mkdir(file)
-            check_folder(os.path.join(folder_path, file))
-
-
-# from distutils.dir_util import copy_tree
-
-if __name__ == "__main__" :
-    src_folder = parcourir(title="Choose a folder to copy")
-    dest_folder = parcourir(title="Choose a folder to paste")
-    files = os.listdir(src_folder)
-    # print(files)
-    for file in files :
-        if os.path.isfile(os.path.join(src_folder, file)) :
-            shutil.copy(os.path.join(src_folder, file), dest_folder)
+        if os.path.isfile(os.path.join(src_folder_path, file)) :
+            ## Check if the file already exists and if the metadata are the same (os.path.getctime)
+            shutil.copy2(os.path.join(src_folder_path, file), os.path.join(dest_folder_path, file))
             print(file, "copied")
         else : 
-            files = check_folder(os.path.join(src_folder, file), dest_folder)
+            os.mkdir(os.path.join(dest_folder_path, file))
+            # print(os.path.join(dest_folder_path, file))
+            check_folder(os.path.join(src_folder_path, file), os.path.join(dest_folder_path, file))
+
+if __name__ == "__main__" :
+    src_folder = parcourir()
+    dest_folder = parcourir(src=False)
+    check_folder(src_folder_path=src_folder, dest_folder_path=dest_folder)
